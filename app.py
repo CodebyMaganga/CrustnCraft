57db6dc7c9e12d8a1cdcd507b54ac1369e349f9c
Maganga Mwambonu <codebymaganga@gmail.com>
2024-01-21 18:43:25 +0300

    Landing page completed

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, make_response, jsonify, request
+from flask_restful import Resource, Api
 from flask_migrate import Migrate
-from models import db, Restaurant,Pizza, restaurant_pizza
+from models import db, Restaurant,Pizza
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app,db)
 
+api= Api(app)
 db.init_app(app)
 
 
@@ -20,9 +25,17 @@ def index():
 
 @app.route('/restaurants')
 def get_restaurants():
-    restaurants = [restaurant.to_dict() for restaurant in Restaurant.query.all()]
+    restaurants =  Restaurant.query.all()
+    restaurant_lists = []
 
-    response = make_response(jsonify(restaurants), 200)
+    for restaurant in restaurants:
+        restaurant_dict={
+            "name": restaurant.name,
+            "address": restaurant.address
+        }
+        restaurant_lists.append(restaurant_dict)
+
+    response = make_response(jsonify(restaurant_lists), 200)
 
     return response
 
@@ -46,7 +59,7 @@ def get_restaurant(id):
 
             return jsonify({"message": "item deleted succesfully"})
              
-
+"""
 @app.route('/pizzas')
 def all_pizzas():
     pizza = [pizza.to_dict() for pizza in Pizza.query.all()]
@@ -54,8 +67,27 @@ def all_pizzas():
     response=make_response(jsonify(pizza), 200)
 
     return response
+"""
+class GetPizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        pizza_list = []
+        for pizza in pizzas:
+            pizza_dict = {
+                'id': pizza.id,
+                'name': pizza.name,
+                'ingredients': pizza.ingredients,
+                'restaurant_id': pizza.restaurant_id,
+            }
+            pizza_list.append(pizza_dict)
+        response = make_response(jsonify(pizza_list), 200)
+        return response
 
-@app.route('/restaurant_pizzas', methods=['POST'])
+api.add_resource(GetPizzas, '/pizzas')
+
+
+"""
+ @app.route('/restaurant_pizzas', methods=['POST'])
 def create_pizzeria():
     data = request.get_json()
 
@@ -76,6 +108,10 @@ def create_pizzeria():
     
 
     return make_response(jsonify(new_pizza.to_dict()), 200)
+
+    }
+"""
+   
 
         
 if __name__ == "__main__":
